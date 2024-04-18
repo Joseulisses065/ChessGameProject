5be43af10d9aca30a781e016e9eb0cfb6de6a81b
joseulisses065 <sulisses111@gmail.com>
2024-04-18 16:53:58 -0300

    Task: BoadException and defensive programming

--- src/br/com/chessGameProject/bordergame/Board.java
+++ src/br/com/chessGameProject/bordergame/Board.java
@@ -7,6 +7,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if(rows < 1 || columns<1){
+            throw new BordException("Error creating board: there must be at last 1 row an 1 column");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -21,17 +24,43 @@ public class Board {
     }
 
     public Piece piece(int row, int column){
+        if(!positionExists(row,column)){
+            throw new BordException("Error position not on the board");
+        }
+
         return pieces[row][column];
     }
 
     public Piece piece(Position position){
+        if(!positionExists(position)){
+            throw new BordException("Error position not on the board");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePice(Piece piece, Position position){
+        if (therelsAPiece(position)){
+            throw new BordException("There is already a piece on position"+position);
+
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
 
+    }
+
+    private boolean positionExists(int row, int column){
+        return row>=0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(),position.getColumn());
+    }
+
+    public boolean therelsAPiece(Position position){
+        if(!positionExists(position)){
+            throw new BordException("Error position not on the board");
+        }
+        return piece(position)!= null;
     }
 
 
---
+++ src/br/com/chessGameProject/bordergame/BordException.java
@@ -0,0 +1,7 @@
+package br.com.chessGameProject.bordergame;
+
+public class BordException extends RuntimeException{
+    public BordException(String msg) {
+
+    super(msg);}
+}
