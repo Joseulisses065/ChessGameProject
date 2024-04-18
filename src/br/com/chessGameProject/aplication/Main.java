acf8da3f1af33e847d0520932ce42e3511661490
joseulisses065 <sulisses111@gmail.com>
2024-04-18 15:44:39 -0300

    Task:start to implement board and pieces

--- src/br/com/chessGameProject/aplication/Main.java
+++ src/br/com/chessGameProject/aplication/Main.java
@@ -1,11 +1,10 @@
 package br.com.chessGameProject.aplication;
 
+import br.com.chessGameProject.bordergame.Board;
 import br.com.chessGameProject.bordergame.Position;
 
 public class Main {
     public static void main(String[] args) {
-        Position position = new Position(0,0);
-
-        System.out.println(position);
+        Board bord = new Board(8,8);
     }
 }
---
+++ src/br/com/chessGameProject/bordergame/Piece.java
@@ -0,0 +1,14 @@
+package br.com.chessGameProject.bordergame;
+
+public class Piece {
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+}
