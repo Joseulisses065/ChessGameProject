a8a3983bd8c7a8df1c03decb842a0458b0325e28
joseulisses065 <sulisses111@gmail.com>
2024-04-18 15:37:01 -0300

    first class: position

---
+++ src/br/com/chessGameProject/aplication/Main.java
@@ -0,0 +1,11 @@
+package br.com.chessGameProject.aplication;
+
+import br.com.chessGameProject.bordergame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+        Position position = new Position(0,0);
+
+        System.out.println(position);
+    }
+}
